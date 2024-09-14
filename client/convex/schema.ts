b06b9b89bbb7047ba092d430fb5d0e1a5f72ecb9
Abhi Arya <abhigyaa@uci.edu>
2024-09-14 11:27:37 -0400

    init db schema

---
+++ client/convex/schema.ts
@@ -0,0 +1,32 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+    project: defineTable({
+        creator: v.id("user"),
+        name: v.string(), 
+        shared_users: v.array(v.string()),
+        blocks: v.optional(v.any()),
+        code: v.optional(v.string()),
+        description: v.optional(v.string()),
+        embedding: v.array(v.float64()),
+    })
+    .index('byCreator', ['creator'])
+    .index('bySharedUsers', ['shared_users'])
+    .vectorIndex("by_embedding", {
+        vectorField: "embedding",
+        dimensions: 1536,
+        filterFields: ["description"],
+      }),
+
+    user: defineTable({
+        user_id: v.string(),
+        pfp_url: v.string(), 
+        email: v.string(), 
+        name: v.string(), 
+    })
+    .index("byUserId", ["user_id"])
+    .index("byEmail", ["email"])
+    .index("byName", ["name"])
+
+});
