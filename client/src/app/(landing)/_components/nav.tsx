ad05274ad380cb1961e4af80ddbba463db873036
Abhi Arya <abhigyaa@uci.edu>
2024-09-14 12:01:48 -0400

    fix styling with nav

--- client/src/app/(landing)/_components/nav.tsx
+++ client/src/app/(landing)/_components/nav.tsx
@@ -48,13 +48,13 @@ const Navbar = () => {
                         {isAuthenticated && !isLoading && (
                             <div className="flex flex-row grid-cols-2 gap-x-4">
                             <a href="/dashboard" className="pr-1">
-                                <button className="px-2 rounded-sm w-full py-1 border-2  mr-1 mb-1 border-black dark:border-white dark:bg-black dark:text-white uppercase bg-white font-semibold text-black transition duration-200 text-sm dark:hover:shadow-[3px_3px_0px_0px_rgba(255,255,255)] hover:shadow-[3px_3px_0px_0px_rgba(0,0,0)] shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] ">
-                                    Enter Opennote
-                                </button>
+                                <Button>
+                                    Enter Blocks
+                                </Button>
                             </a>
-                            <div className="rounded-full">
-                            <UserButton />
-                            </div>
+                                <div className="rounded-full pt-1 pr-2">
+                                    <UserButton />
+                                </div>
                             </div>
                         )}
                     </div>
