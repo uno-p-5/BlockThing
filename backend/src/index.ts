cf9b31e6529f6f60fa475b3339596744aab3eecb
Abhi Arya <abhigyaa@uci.edu>
2024-09-14 11:33:50 -0400

    add dev:nr no-autoreload script

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,7 @@ import cors from "cors";
 dotenv.config(); 
 
 const APP: Express = express(); 
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 8080; 
 
 APP.use(cors());
 APP.use(express.json());
