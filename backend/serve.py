d3dc7b3c6cd0ef9b9fd37cc339924ab197a8410b
Abhi Arya <abhigyaa@uci.edu>
2024-09-14 11:31:05 -0400

    init nodejs backend

--- backend/serve.py
+++
@@ -1,5 +0,0 @@
-import uvicorn 
-
-
-if __name__ == "__main__":
-    uvicorn.run("app:app", port=8080, reload=True)
---
+++ backend/src/index.ts
@@ -0,0 +1,21 @@
+import express, { Express, Request, Response } from "express"; 
+import dotenv from "dotenv"; 
+import cors from "cors";
+
+dotenv.config(); 
+
+const APP: Express = express(); 
+const PORT = process.env.PORT || 3000; 
+
+APP.use(cors());
+APP.use(express.json());
+
+APP.get('/', (req: Request, res: Response) => { 
+	res.send({
+		"message": "Hello from Block Thing"
+	}); 
+}); 
+
+APP.listen(PORT, () => {
+  console.log(`[server]: Server is running at http://localhost:${PORT}`);
+});
