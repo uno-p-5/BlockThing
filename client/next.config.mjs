f58a8a3f45b77f1e3510586c9a304e475e010a36
Kevin Wu <kevinwu098@gmail.com>
2024-09-14 21:30:46 -0400

    feat: whisper

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    // Ignore node-specific modules when bundling for the browser
+    // https://webpack.js.org/configuration/resolve/#resolvealias
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      sharp$: false,
+      "onnxruntime-node$": false,
+    };
+    return config;
+  },
+  reactStrictMode: false,
   images: {
     remotePatterns: [
       {
